51fd31826a4a709545c1a7612bdb6d1231a7888d
Andrey <andrios2003@gmail.com>
2022-07-22 19:46:39 +0200

    tabs add, without script

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
+/* $(document).ready(function(){
     $('.carousel__inner').slick({
       speed: 1300,
-      prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
-      nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
+      prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
       autoplay: false,
       responsive: [{
         breakpoint: 1070,
@@ -13,4 +13,38 @@ $(document).ready(function(){
         }
       }]
     });
+  }); */
+  const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: '1',
+    autoplay: false,
+    controls: false,
+    nav:false,
+    navPosition: 'bottom',
+    mouseDrag: true,
+    loop: true,
+    speed: 1200,
+    responsive: {
+      993: {
+
+      }, 
+      767: {
+        nav: false
+      }, 
+      576: {
+
+      },
+      320: {
+        nav: true,
+      }
+    }
   });
+
+  document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+  });
+
+  document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+  }); 
