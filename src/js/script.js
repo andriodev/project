a34931b398b040afc19ed4f9a012e416632ec486
Andrey <andrios2003@gmail.com>
2022-07-29 12:57:50 +0200

    modal windows ready

--- src/js/script.js
+++ src/js/script.js
@@ -58,14 +58,6 @@
 			.closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
 	});
 
-
-	/* $('.catalog-item__link').each(function(i) {
-		$(this).on('click', function() {
-			$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-			$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-		})
-	}); */
-
 	function togleSlide(item) {
 		$(item).each(function(i) {
 			$(this).on('click', function(e) {
